8439d39f0cfd25e4fbba9ec7dc308f99dfd6ef05
The Aviator <abhi@xyz>
2016-01-07 06:05:09 +0530

    Adding functionalities:
    Adding Note to SQLITE Database on back of edit note screen
    querying the database for notes with timestamp order latest first
    star button added with glitches

--- app/src/main/java/cook/cuu/feathernotes/EditNotes.java
+++ app/src/main/java/cook/cuu/feathernotes/EditNotes.java
@@ -1,11 +1,19 @@
 package cook.cuu.feathernotes;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditNotes extends AppCompatActivity {
+
+    EditText editText;
+    DbHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +23,60 @@ public class EditNotes extends AppCompatActivity {
         Intent intent = getIntent();
         int position = intent.getIntExtra("noteId", -1);
 
-        EditText editText = (EditText) findViewById(R.id.editText);
+        editText= (EditText) findViewById(R.id.editText);
 
         if(position!= -1)
-            editText.setText(MainActivity.data[position]);
+            editText.setText(MainActivity.data.get(position));
         else
             editText.setText("");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Life", "onStop Called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Life", "onPause Called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Life", "onRestart Called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Life", "onResume Called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+//        try {
+            Log.i("Life", "onDestroy Called");
+            if (TextUtils.isEmpty(editText.getText())) {
+                Toast.makeText(this, "No Text Typed", Toast.LENGTH_LONG).show();
+                Log.i("Life", "No Text Typed");
+            } else {
+
+                dbHelper = new DbHelper(this);
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                SQLiteStatement stmt = db.compileStatement("INSERT INTO notes (noteText,star) values (?,0);");
+                stmt.bindString(1, String.valueOf(editText.getText()));
+                stmt.execute();
+
+                Toast.makeText(this, "Note Inserted ..", Toast.LENGTH_SHORT).show();
+                Log.i("Life","Note Inserted ..");
+            }
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
     }
 }
--- app/src/main/java/cook/cuu/feathernotes/MainActivity.java
+++ app/src/main/java/cook/cuu/feathernotes/MainActivity.java
@@ -1,24 +1,31 @@
 package cook.cuu.feathernotes;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-    static String[] data = {"Jan","Feb","Mar","April","May","June","July","Aug","Sept","Oct"};
+    //static String[] data = {"Jan","Feb","Maraaaaa aaaaa aaaaa aaaa aaaaa aaaaa aaaaa aaaaa aaa aaaaaa","April","May","June","July","Aug","Sept","Oct"};
+    static ArrayList<String> data ;
     ListView listView;
+    static SQLiteDatabase notesDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,33 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        try {
+            data = new ArrayList<>();
+            notesDB = this.openOrCreateDatabase("featherNotesDB", MODE_PRIVATE, null);
+            notesDB.execSQL("CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, noteText VARCHAR, " +
+                    "star INTEGER DEFAULT 0,timestamp DATE DEFAULT (datetime('now','localtime')));");
 
+            Cursor c = notesDB.rawQuery("SELECT id,noteText,star from notes ORDER BY timestamp DESC",null);
+
+            int noteIdIndex = c.getColumnIndex("id");
+            int noteIndex = c.getColumnIndex("noteText");
+            int starIndex = c.getColumnIndex("star");
+
+            c.moveToFirst();
+
+
+            while(c!=null){
+                Log.i("dabo","noteId ->" + Integer.toString(c.getInt(noteIdIndex)));
+                Log.i("dabo","note ->" + c.getString(noteIndex));
+                Log.i("dabo","star ->" + Integer.toString(c.getInt(starIndex)));
+                data.add(c.getString(noteIndex));
+                c.moveToNext();
+            }
+
+
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,12 +75,22 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.single_row,R.id.noteTextView,data);
         listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(this);
+        arrayAdapter.setNotifyOnChange(true);
     }
 
     public void callEditNote(int position){
         Intent intent = new Intent(this,EditNotes.class);
-        intent.putExtra("noteId",position);
+        intent.putExtra("noteId", position);
         startActivity(intent);
+    }
+
+    public void onToggleStar(View view){
+        if(view.getId() == R.id.favorite)
+            Log.i("star", "This is star toggle");
+
+        Toast.makeText(this,"You're a star !!",Toast.LENGTH_LONG).show();
+
+
     }
 
     @Override
